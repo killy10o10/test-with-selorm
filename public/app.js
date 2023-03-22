edc1f0c897b389117dd4ef6598587f195c490944
Justice Selorm Bruec <JusticeSelormBruce@gmail.com>
2023-03-22 17:33:28 +0000

    refactor app.js to listen to DomContentLoaded  event before implementing  css classes

--- public/app.js
+++ public/app.js
@@ -1,1 +1,93 @@
-if(document.cookie.indexOf("jwt_exist")>-1){const classList=[".login",".register",".logout",".dashbord"];classList.forEach(val=>{if(val===".login"||val===".register"){document.querySelector(val).classList.add("hidden")}else{document.querySelector(val).classList.remove("hidden")}})}if(document.cookie.indexOf("jwt_exist")>-1){location.assign("/dashbord")}const signIn=function(){var form=document.querySelector("form");var usernameError=document.querySelector(".error.username");var passwordeError=document.querySelector(".error.password");var unauthorizedeError=document.querySelector(".error.unauthorized");form.addEventListener("submit",async e=>{e.preventDefault();var username=form.username.value;var password=form.password.value;console.log(username);var res=await fetch("/sign-in",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({username:username,password:password})});var data=await res.json();console.log(data);if(data.errors){usernameError.textContent=data.errors.username;passwordeError.textContent=data.errors.password;console.log(data.errors.unauthorized);unauthorizedeError.textContent=data.errors.unauthorized}if(data.user){location.assign("/dashbord")}})};const signUp=function(){var form=document.querySelector("form");var usernameError=document.querySelector(".error.username");var passwordeError=document.querySelector(".error.password");var tosAgreementError=document.querySelector(".error.tosAgreement");form.addEventListener("submit",async e=>{e.preventDefault();var username=form.username.value;var password=form.password.value;var tosAgreement=form.tosAgreement.checked;var res=await fetch("/sign-up",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({username:username,password:password,tosAgreement:tosAgreement})});var data=await res.json();if(data.errors){usernameError.textContent=data.errors.username;passwordeError.textContent=data.errors.password;tosAgreementError.textContent=data.errors.tosAgreement}if(data.user){location.assign("/dashbord")}})};
+/**
+ * font-end javascript code
+ */
+
+// check to see if jwt_exist=true,meaning user is logged in so hide login and registeer button
+if (document.cookie.indexOf("jwt_exist") > -1) {
+  document.addEventListener("DOMContentLoaded", function () {
+    const classList = [".login", ".register", ".logout", ".dashbord"];
+    classList.forEach((val)=>{
+        if(val === '.login' || val === '.register'){
+            document.querySelector(val).classList.add('hidden')
+        }else{
+            document.querySelector(val).classList.replace('hidden','show')
+        }
+    })
+  });
+}
+
+//sign-in javacript logic
+const signIn = function () {
+  var form = document.querySelector("form");
+
+  //error fields
+  var usernameError = document.querySelector(".error.username");
+  var passwordeError = document.querySelector(".error.password");
+  var unauthorizedeError = document.querySelector(".error.unauthorized");
+
+  //listen to submit event
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    //get input
+    var username = form.username.value;
+    var password = form.password.value;
+    console.log(username);
+
+    //submit form
+    var res = await fetch("/sign-in", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    });
+    var data = await res.json();
+    console.log(data);
+
+    if (data.errors) {
+      usernameError.textContent = data.errors.username;
+      passwordeError.textContent = data.errors.password;
+
+      unauthorizedeError.textContent = data.errors.unauthorized;
+    }
+    if (data.user) {
+      location.assign("/dashbord");
+    }
+  });
+};
+
+//signup javascript logic
+const signUp = function () {
+  var form = document.querySelector("form");
+
+  //error fields
+  var usernameError = document.querySelector(".error.username");
+  var passwordeError = document.querySelector(".error.password");
+  var tosAgreementError = document.querySelector(".error.tosAgreement");
+
+  //listen to submit event
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    //get input
+    var username = form.username.value;
+    var password = form.password.value;
+    var tosAgreement = form.tosAgreement.checked;
+
+    //submit form
+    var res = await fetch("/sign-up", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password, tosAgreement }),
+    });
+    var data = await res.json();
+
+    if (data.errors) {
+      usernameError.textContent = data.errors.username;
+      passwordeError.textContent = data.errors.password;
+      tosAgreementError.textContent = data.errors.tosAgreement;
+    }
+    if (data.user) {
+      location.assign("/dashbord");
+    }
+  });
+};
