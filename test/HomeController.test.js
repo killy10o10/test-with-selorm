dcf89f02a708096ea287edaf03a328a4e64b6bbd
Justice Selorm Bruec <JusticeSelormBruce@gmail.com>
2023-03-25 16:04:02 +0000

    Update HomeController.test.js

--- test/HomeController.test.js
+++ test/HomeController.test.js
@@ -16,7 +16,7 @@ const app = require("../index");
 describe("HomeController", () => {
   let server;
   beforeAll(() => {
-    server = app.listen(3000);
+    server = app.listen(3001);
   });
   afterAll((done) => {
     server.close();
